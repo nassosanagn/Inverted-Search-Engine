ca0d8817a3ac42666fc822a57fa2eaf75a8b35a3
TollisK <tolliskarv@gmail.com>
2021-10-22 14:50:23 +0300

    Fix word struct

--- main_folder/entry.h
+++ main_folder/entry.h
@@ -1,5 +1,6 @@
 #include <cstring>
 #include <stdlib.h>
+#include <iostream>
 enum error_code
 {
     SUCCESS = 0,
@@ -10,11 +11,16 @@ class word {
     char* String;
     public:
         word(char* tmp){
+            String = new char[strlen(tmp)]();
             strcpy(String,tmp);
+        }
+        ~word(){
+            delete String;
         }
         char* getword() const{ 
             return String; 
         }
+
 };
 
 class entry {
@@ -61,18 +67,18 @@ class entry_list{
         }
 };
 
-enum error_code create_entry(const word* w, entry* e);
+enum ::error_code create_entry(const word* w, entry* e);
 
-enum error_code destroy_entry(entry *e);
+enum ::error_code destroy_entry(entry *e);
 
-enum error_code create_entry_list(entry_list* el);
+enum ::error_code create_entry_list(entry_list* el);
 
 unsigned int get_number_entries(const entry_list* el);
 
-enum error_code add_entry(entry_list** el, const entry* e);
+enum ::error_code add_entry(entry_list** el, const entry* e);
 
 entry* get_first(const entry_list* el);
 
 entry* get_next(const entry_list* el);
 
-enum error_code destroy_entry_list(entry_list* el);
+enum ::error_code destroy_entry_list(entry_list* el);
