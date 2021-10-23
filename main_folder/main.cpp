5a9238bf08c5937aba700cd8e5cef904da1b2a02
TollisK <tolliskarv@gmail.com>
2021-10-23 15:49:17 +0300

    fixed char_dif function

--- main_folder/main.cpp
+++ main_folder/main.cpp
@@ -3,9 +3,9 @@
 #include <iostream>
 using namespace std;
 
-int char_dif(char* word1,char* word2){
+int char_dif(const char* word1,const char* word2){
     int j = 0;
-    for(int i=0;i<strlen(word1);i++){
+    for(int i=0;i<strlen(strlen(word1)>strlen(word2)? word1 : word2);i++){
         if(word1[i]!=word2[i]){
             j++;
         }
@@ -15,7 +15,7 @@ int char_dif(char* word1,char* word2){
 
 int main(int argc, char* argv[]){
     char tmp1[] = "hell";
-    char tmp2[] = "fall";
+    char tmp2[] = "fallasd";
     cout << char_dif(tmp1, tmp2)<<endl;
     return 0;
 }
