b91ac8831c40f566f54f945b36551943014d75ce
TollisK <tolliskarv@gmail.com>
2021-10-23 10:52:48 +0300

    added character diff function to main

--- main_folder/main.cpp
+++ main_folder/main.cpp
@@ -1,9 +1,21 @@
 #include <cstdlib>
-#include <cstdio>
+#include <cstring>
+#include <iostream>
 using namespace std;
 
+int char_dif(char* word1,char* word2){
+    int j = 0;
+    for(int i=0;i<strlen(word1);i++){
+        if(word1[i]!=word2[i]){
+            j++;
+        }
+    }
+    return j;
+}
 
 int main(int argc, char* argv[]){
-
+    char tmp1[] = "hell";
+    char tmp2[] = "fall";
+    cout << char_dif(tmp1, tmp2)<<endl;
     return 0;
 }
