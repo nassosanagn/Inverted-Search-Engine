22dfb0b7d6ba2e4bbd9ceb1a95793d1ab8698689
GenJohny <johnythegeneral@gmail.com>
2021-11-15 20:04:45 +0200

    fixed a bug where the tree was inserting incorrectly

--- main_folder/BKTree.cpp
+++ main_folder/BKTree.cpp
@@ -34,9 +34,10 @@ ErrorCode Index::destroy_entry_index(treeNode* ix){
 
 //Synarthsh gia eyresh hamming distance 
 int Index::HammingDistance(const char* word1, const char* word2){
-        int j = 0;
-        for(int i=0;i<(strlen(word1) > strlen(word2) ? strlen(word1) : strlen(word2));i++){
-            if(word1[i]!=word2[i]){
+        int j = strlen(word1)-strlen(word2);
+        if (j < 0){j=-j;}
+        for(int i=0;i<(strlen(word1) < strlen(word2) ? strlen(word1) : strlen(word2));i++){
+            if(word1[strlen(word1)-i-1]!=word2[strlen(word2)-i-1]){
                 j++;
             }
         }
@@ -134,7 +135,7 @@ ErrorCode Index::insertTree(char* str, char* cmpWord, treeNode* tempNode, MatchT
     //An den yparxei allo paidi ths rizas 
     if (this->getRoot()->getChildNode() == NULL){                     // Only for the first time 
         getRoot()->setChildNode(new treeNode(str,tempDiff));
-        cout << "Inserted help" << endl; 
+        cout << "Inserted help returning" << endl; 
         return EC_SUCCESS;
     }
 
@@ -145,6 +146,14 @@ ErrorCode Index::insertTree(char* str, char* cmpWord, treeNode* tempNode, MatchT
     if ((tempDiff == tempNode->getDiff())) {        /* Go down on that node */
 
         if (tempNode->getChildNode() == NULL){
+            switch(matchtype){
+                case MT_HAMMING_DIST:
+                    tempDiff = HammingDistance(str, tempNode->getString());
+                    break;
+                case MT_EDIT_DIST:
+                    tempDiff = EditDistance(str,strlen(str),tempNode->getString(),strlen(tempNode->getString()));
+                    break;
+            }
             tempNode->setChildNode(new treeNode(str, tempDiff));
             cout << "Inserted " << str << " down after: " << tempNode->getString() << endl;
         }else{
@@ -285,29 +294,49 @@ ErrorCode Index::lookup_entry_index(const word* w, Index* ix, int threshold, ent
 
 int main(){
 
-    char tmpStr[20];
 
+
+
+    
+    char* tmpStr = new char[strlen("hell")];
     strcpy(tmpStr,"hell");
     entry* E = new entry(tmpStr);
     entry_list* El;
 
-    strcpy(tmpStr,"help");
-    entry* E1 = new entry(tmpStr);
+    char* tmpStr1 = new char[strlen("help")];
+    strcpy(tmpStr1,"help");
+    entry* E1 = new entry(tmpStr1);
 
-    strcpy(tmpStr,"fall");
-    entry* E2 = new entry(tmpStr);
 
-    strcpy(tmpStr,"felt");
-    entry* E3 = new entry(tmpStr);
+    char* tmpStr2 = new char[strlen("fall")];
+    strcpy(tmpStr2,"fall");
+    entry* E2 = new entry(tmpStr2);
 
-    strcpy(tmpStr,"fell");
-    entry* E4 = new entry(tmpStr);
 
-    strcpy(tmpStr,"small");
-    entry* E5 = new entry(tmpStr);
 
-    strcpy(tmpStr,"melt");
-    entry* E6 = new entry(tmpStr);
+    char* tmpStr3 = new char[strlen("felt")];
+    strcpy(tmpStr3,"felt");
+    entry* E3 = new entry(tmpStr3);
+
+
+    char* tmpStr4 = new char[strlen("fell")];
+    strcpy(tmpStr4,"fell");
+    entry* E4 = new entry(tmpStr4);
+
+
+    char* tmpStr5 = new char[strlen("small")];
+    strcpy(tmpStr5,"small");
+    entry* E5 = new entry(tmpStr5);
+
+
+    char* tmpStr6 = new char[strlen("melt")];
+    strcpy(tmpStr6,"melt");
+    entry* E6 = new entry(tmpStr6);
+
+    char* tmpStr7 = new char[strlen("small")];
+    strcpy(tmpStr7,"small");
+    entry* E7 = new entry(tmpStr7);
+    
 
 
     El->create_entry_list(&El);
@@ -318,20 +347,16 @@ int main(){
     El->add_entry(El,E4);
     El->add_entry(El,E5);
     El->add_entry(El,E6);
+    El->add_entry(El,E7);
 
-    delete E;
-    delete E1;
-    delete E2;
-    delete E3;
-    delete E4;
-    delete E5;
-    delete E6;
+
+    
 
     Index* Index1 = new Index();
     Index1->build_entry_index(El,MT_HAMMING_DIST,Index1);
-    
-    El->destroy_entry_list(&El);
-    delete El;
+
+
+
 
     //Testing print
     for (size_t i = 0; i < 10; i++)
@@ -348,13 +373,24 @@ int main(){
     int threshold = 2;
     entry_list* result= new entry_list();
 
-    // Index1->lookup_entry_index(&myword,Index1,threshold,result);
+    Index1->lookup_entry_index(&myword,Index1,threshold,result);
 
     cout<<"print start \n";
     result->print_list(result);
 
     result->destroy_entry_list(&result);
 
+    delete E;
+    delete E1;
+    delete E2;
+    delete E3;
+    delete E4;
+    delete E5;
+    delete E6;
+    
+
+    El->destroy_entry_list(&El);
+    delete El;
     delete result;
     delete Index1;
     return 0;
